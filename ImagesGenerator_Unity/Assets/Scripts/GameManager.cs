6dc11efecae01a83058344217060e34d6a4394f1
Celia <celica02@ucm.es>
2021-01-19 12:27:43 +0100

    Images at runtime succesfully created and saved

--- ImagesGenerator_Unity/Assets/Scripts/GameManager.cs
+++ ImagesGenerator_Unity/Assets/Scripts/GameManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    string m_Path;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Get the path of the Game data folder
+        m_Path = Application.dataPath + "/ImagesCreated";
+
+        //Output the Game data path to the console
+        Debug.Log("dataPath : " + m_Path);
+
+        if (!System.IO.Directory.Exists(m_Path))
+            System.IO.Directory.CreateDirectory(m_Path);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
+            ScreenShot.TakeCameraScreenshot(600, 600, m_Path);
     }
 }
--- ImagesGenerator_Unity/Assets/Scripts/ScreenShot.cs
+++ ImagesGenerator_Unity/Assets/Scripts/ScreenShot.cs
@@ -4,9 +4,54 @@ using UnityEngine;
 
 public class ScreenShot : MonoBehaviour
 {
-    void OnMouseDown()
+    private static ScreenShot instance;
+
+    private Camera myCamera;
+    private bool takeScreenShotOnNextFrame;
+    private string path;
+
+    private void Awake()
     {
-        ScreenCapture.CaptureScreenshot("Prueba1.png");
-        Debug.Log("Captura de pantalla");
+        instance = this;
+        myCamera = gameObject.GetComponent<Camera>();
+     
     }
+
+    private void OnPostRender()
+    {
+        if (takeScreenShotOnNextFrame)
+        {
+            takeScreenShotOnNextFrame = false;
+            RenderTexture renderTexture = myCamera.targetTexture;
+
+            Texture2D renderResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+            Rect rect = new Rect(0, 0, renderTexture.width, renderTexture.height);
+            renderResult.ReadPixels(rect, 0, 0);
+
+
+            byte[] byteArray = renderResult.EncodeToPNG();
+            System.IO.File.WriteAllBytes(path + "/Photo.png", byteArray);
+            Debug.Log("Created 'Photo.png'");
+            RenderTexture.ReleaseTemporary(renderTexture);
+            myCamera.targetTexture = null;
+        }
+
+    }
+
+    private void TakeScreenshots(int width, int height, string savingPath)
+    {
+        myCamera.targetTexture = RenderTexture.GetTemporary(width, height, 16);
+        takeScreenShotOnNextFrame = true;
+        path = savingPath;
+    }
+
+    public static void TakeCameraScreenshot (int width, int height, string savingPath)
+    {
+        instance.TakeScreenshots(width, height, savingPath);
+    }
+    //void OnMouseDown()
+    //{
+    //    ScreenCapture.CaptureScreenshot("Prueba1.png");
+    //    Debug.Log("Captura de pantalla");
+    //}
 }
