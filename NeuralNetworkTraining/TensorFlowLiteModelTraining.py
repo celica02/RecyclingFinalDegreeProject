138aac1d62ac93b0676dbdc00f31e5b19543d4c3
Celia <celica02@ucm.es>
2021-06-15 13:32:09 +0200

    Directories organized and images added

---
+++ NeuralNetworkTraining/TensorFlowLiteModelTraining.py
@@ -0,0 +1,41 @@
+#-------------------------------------------------------------------------------
+# Name:        TensorflowModel
+# Purpose:
+#
+# Author:      Celia
+#
+# Created:     03/12/2020
+# Copyright:   (c) Celia 2020
+#---------------------------
+#----------------------------------------------------
+import numpy as np
+
+import tensorflow as tf
+assert tf.__version__.startswith('2')
+
+from tensorflow_examples.lite.model_maker.core.data_util.image_dataloader import ImageClassifierDataLoader
+from tensorflow_examples.lite.model_maker.core.task import image_classifier
+#from tensorflow_examples.lite.model_maker.core.task.model_spec import mobilenet_v2_spec
+#from tensorflow_examples.lite.model_maker.core.task.model_spec import ImageModelSpec
+
+import matplotlib.pyplot as plt
+
+#../Recursos/Images/ParaRedNeuronal/Train/
+
+train_data = ImageClassifierDataLoader.from_folder('../Resources/Images/Train/')
+print('Imágenes de entrenamiento cargadas')
+
+test_data = ImageClassifierDataLoader.from_folder('../Resources/Images/Test/')
+#train_data, test_data = data.split(0.9)
+print('Imágenes de test cargadas')
+
+model = image_classifier.create(train_data)
+print('Modelo entrenado')
+
+loss, accuracy = model.evaluate(test_data)
+#print('Test accuracy: %f' % accuracy )
+
+#model.export('Images/')
+model.export(export_dir='../Resources/TrainedModel/', with_metadata=False)
+
+print('Modelo exportado')
